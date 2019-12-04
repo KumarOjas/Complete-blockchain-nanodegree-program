7809ee720567b607b5c0b23e7b232263ba2212de
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2019-12-04 16:21:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express')
 const app = express()
 app.get('/',(req,res)=>res.send('Hello world!'))
 app.listen(3000,()=>console.log('Example app listening on port 3000!'))
+//Express.js, or simply Express, is a web application framework for Node.js, released as free and open-source software under the MIT License. 
+//It is designed for building web applications and APIs.
+//It has been called the de facto standard server framework for Node.js.
