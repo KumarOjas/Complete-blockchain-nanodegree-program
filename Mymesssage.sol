062035dd76b8c3647731e1df37efccd28c0b15b6
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2019-12-05 23:49:35 +0530

    Create Mymesssage.sol

---
+++ Mymesssage.sol
@@ -0,0 +1,14 @@
+
+//This is to deploy your local blockchain
+pragma solidity ^0.4.24
+contract Message {
+    string myMessage;
+    
+    function setMessage(string x) public {
+        myMessage=x;
+        }
+        function getMessage() public view returns (string) {
+           return myMessage;
+           }}
+        
+        
